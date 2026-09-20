b77f647e67a7aba3f8613c0e3afe4ab2038941ef
DarkIzuku <92455495+DarkIzuku@users.noreply.github.com>
2026-09-20 19:14:09 -0400

    bloodborne: expose host summon-search activity

--- src/core/bloodborne_re.h
+++ src/core/bloodborne_re.h
@@ -13,6 +13,7 @@ namespace Core::Bloodborne {
 std::optional<std::string> GetSeamlessHostPlacementHeader();
 bool SetSeamlessHostPlacementHeader(std::string_view value);
 void ClearSeamlessHostPlacementHeader();
+void NotifySeamlessSummonSearchObserved();
 void NotifySeamlessSummonClaimAccepted();
 std::uint64_t NotifySeamlessSummonRoomJoinStarted(std::uint64_t room_id);
 void NotifySeamlessSummonRoomJoined(std::uint64_t room_id);
